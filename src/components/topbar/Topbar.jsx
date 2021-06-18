7acd7138a63bbf36514c887d545c0d24a39cdb66
EWcode-LA <seth.miller87@proton.mail>
2021-06-18 09:27:56 -0700

    additional code

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import "./topbar.scss"
 import {Person,Mail} from "@material-ui/icons"
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
     return (
-        <div className="topbar">
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                <div className="left">
                    <a href="#intro" className="logo">genuis.</a>
@@ -17,7 +17,7 @@ export default function Topbar() {
                    </div>
                 </div> 
                 <div className="right">
-                    <div className="hamburger">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
                         <span className="line1"></span>
                         <span className="line2"></span>
                         <span className="line3"></span>
