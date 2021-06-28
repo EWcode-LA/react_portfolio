2669df42b050bad934156d4b13455a196d767beb
EWcode-LA <seth.miller87@proton.mail>
2021-06-28 15:35:39 -0700

    mas improvements

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,23 +1,40 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
-    return (
-        <div className="intro" id="intro">
-            <div className="left">
-                <div className="imgContainer">
-                    <img src="assets/IMG_3368.jpg" alt="" />
-                </div>
-            </div>
-            <div className="right">
-                <div className="wrapper">
-                    <h2>Hi There, I'm</h2>
-                    <h1>Seth Miller</h1>
-                    <h3>Full-Stack<span></span></h3>
-                </div>
-                <a href="#portfolio">
-                    <img src="" alt="" />
-                </a>
-            </div>
+  const textRef = useRef();
+
+  //used function instead of component from ityped-npm example
+  useEffect(() => {
+    // console.log(textRef)
+    init(textRef.current, {
+      showCursor: false,
+      //add blinking cursor to CSS and change styling
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Engineer", "Freelance"],
+    });
+  }, []);
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/IMG_3368.jpg" alt="" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Seth Miller</h1>
+          <h3>
+            Full-Stack <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down_arrow.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,59 @@
-import "./portfolio.scss"
+import PortfolioList from "../portfolioList/PortofolioList";
+import "./portfolio.scss";
+import { useEffect, useState } from "react";
 
+//change li tags to what's appropriate for your portfolio
 export default function Portfolio() {
-    return (
-        <div className="portfolio" id="portfolio">
-            Add portfolio
+  const [selected, setSelected] = useState("featured");
+
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+
+    {
+      id: "web",
+      title: "Web App",
+    },
+
+    {
+      id: "Mobile",
+      title: "Mobile App",
+    },
+
+    {
+      id: "design",
+      title: "Design",
+    },
+
+    {
+      id: "branding",
+      title: "Branding",
+    },
+  ];
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://www.thedroidsonroids.com/wp-content/uploads/2020/04/3-1200x640.png.webp"
+            alt=""
+          />
+          <h3>Future Project</h3>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
